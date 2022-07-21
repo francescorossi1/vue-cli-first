54da68a50a65df4e394d4f1cc4248f832334f269
francescorossi1 <rossi.francesco.dev@gmail.com>
2022-07-21 16:19:58 +0200

    bonus completed

---
+++ src/components/Hero.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h2>Questo è il mio Jumbotron</h2>
+    </div>
+</template>
+
+<script>
+    export default{
+        name: 'MainJumbotron'
+    }
+</script>
+
+<style scoped>
+    div{
+        background-color: aquamarine;
+        height: 80vh;
+        max-width: 90%;
+        margin: 0 auto;
+    }
+    h2{
+        text-align: center;
+        line-height: 80vh;
+        color: white;
+        font-size: 48px;
+    }
+</style>
