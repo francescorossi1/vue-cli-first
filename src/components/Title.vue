d1bfec0830b00c69f65f21d2ddf0539cbd760398
francescorossi1 <rossi.francesco.dev@gmail.com>
2022-07-21 15:49:19 +0200

    completed

---
+++ src/components/Title.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>La mia prima app con Vue CLI!</h1>
+</template>
+
+<script>
+    export default{
+        name: 'MainTitle'
+    }
+</script>
+
+<style scoped>
+    h1{
+        text-align: center;
+    }
+</style>
